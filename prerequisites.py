98de4f42dccd7d414dd6afb4b70b9c9ed10eeedc
mang330 <mang3330@gmail.com>
2025-06-03 21:22:00 +0900

    라이브러리 추가

---
+++ prerequisites.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.stats import chi2_contingency
+from sklearn.neighbors import LocalOutlierFactor
+import warnings
+warnings.filterwarnings('ignore')
