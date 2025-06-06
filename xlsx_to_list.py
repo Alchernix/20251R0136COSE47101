a1433e390ace66e636aa4aa1de5b77dc1d88a14d
overtop0723 <overtop0723>
2025-06-06 14:32:52 +0900

    디버깅용 코드 추가

--- xlsx_to_list.py
+++ xlsx_to_list.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
-file_path = "data.xlsx"
+file_path = "20251R0136COSE47101/data.xlsx"
+# file_path = "/data.xlsx"
+
 df = pd.read_excel(file_path, header=None)
 
 # Columns C to J, Rows 2 to 44
@@ -13,3 +15,7 @@ for col in range(2, 10):  # C~J
         if pd.notna(cell_value):
             items = {item.strip() for item in str(cell_value).split(',')}
             lectures[col-2][row-1] = items
+
+for col in range(8):
+    for row in range(43):
+        if lectures[col][row]: print('lectures['+ str(col) + '][' + str(row) + ']' + str(lectures[col][row]))
