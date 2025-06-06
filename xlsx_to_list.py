4fc4790bf903570bdf54f1f34f92f7a7e34a9842
overtop0723 <overtop0723>
2025-06-06 17:21:43 +0900

    리스트 lectures를 DataFrame 형태로 생성

--- xlsx_to_list.py
+++ xlsx_to_list.py
@@ -1,21 +1,42 @@
 import pandas as pd
 
-file_path = "20251R0136COSE47101/data.xlsx"
-# file_path = "/data.xlsx"
-
+# file_path = "20251R0136COSE47101/data.xlsx"
+file_path = "data.xlsx"
 df = pd.read_excel(file_path, header=None)
 
-# Columns C to J, Rows 2 to 44
-lectures = [[{} for row in range(43)] for col in range(8)]
+students = 43
+
+# Columns C to J, Rows 2 to {students + 1}
+lectures = [[{} for row in range(students)] for col in range(8)]
 
 
 for col in range(2, 10):  # C~J
-    for row in range(1, 44):  # Rows 2 to 44
+    for row in range(1, students+1):  # Rows 2 to {students + 1}
         cell_value = df.iloc[row, col]
         if pd.notna(cell_value):
             items = {item.strip() for item in str(cell_value).split(',')}
             lectures[col-2][row-1] = items
 
-for col in range(8):
-    for row in range(43):
-        if lectures[col][row]: print('lectures['+ str(col) + '][' + str(row) + ']' + str(lectures[col][row]))
+# Code to debug above code (line 13~18)
+# for col in range(8):
+#     for row in range(students):
+#         if lectures[col][row]: print('lectures['+ str(col) + '][' + str(row) + ']' + str(lectures[col][row]))
+
+# Convert list 'lectures' into DataFrame
+dataframe = {
+    'student_ID': range(students),
+    '1y_1s': [lectures[0][i] for i in range(students)],
+    '1y_2s': [lectures[1][i] for i in range(students)],
+    '2y_1s': [lectures[2][i] for i in range(students)],
+    '2y_2s': [lectures[3][i] for i in range(students)],
+    '3y_1s': [lectures[4][i] for i in range(students)],
+    '3y_2s': [lectures[5][i] for i in range(students)],
+    '4y_1s': [lectures[6][i] for i in range(students)],
+    '4y_2s': [lectures[7][i] for i in range(students)]
+}
+
+df = pd.DataFrame(dataframe)
+
+# Code to debug above code (line 26~36)
+# print(df)
+# print(dataframe)
