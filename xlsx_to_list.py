51feeefe2ea09c4272d034eaf2fcf6cad77997c9
overtop0723 <overtop0723>
2025-06-07 15:23:38 +0900

    본전공생 및 본전공이 아닌 학생들만 따로 분류하여 분석할 수 있도록 구현

--- xlsx_to_list.py
+++ xlsx_to_list.py
@@ -39,13 +39,42 @@ dataframe = {
     '4y_2s': [lectures[7][i] for i in range(students)]
 }
 
-df = pd.DataFrame(dataframe)
-# Code to debug above code (line 42)
+dataframe_primary = {
+    'student_ID': [i for i in range(students) if primary_major[i] == 1],
+    '1y_1s': [lectures[0][i] for i in range(students) if primary_major[i] == 1],
+    '1y_2s': [lectures[1][i] for i in range(students) if primary_major[i] == 1],
+    '2y_1s': [lectures[2][i] for i in range(students) if primary_major[i] == 1],
+    '2y_2s': [lectures[3][i] for i in range(students) if primary_major[i] == 1],
+    '3y_1s': [lectures[4][i] for i in range(students) if primary_major[i] == 1],
+    '3y_2s': [lectures[5][i] for i in range(students) if primary_major[i] == 1],
+    '4y_1s': [lectures[6][i] for i in range(students) if primary_major[i] == 1],
+    '4y_2s': [lectures[7][i] for i in range(students) if primary_major[i] == 1]
+}
+
+dataframe_not_primary = {
+    'student_ID': [i for i in range(students) if primary_major[i] == 0],
+    '1y_1s': [lectures[0][i] for i in range(students) if primary_major[i] == 0],
+    '1y_2s': [lectures[1][i] for i in range(students) if primary_major[i] == 0],
+    '2y_1s': [lectures[2][i] for i in range(students) if primary_major[i] == 0],
+    '2y_2s': [lectures[3][i] for i in range(students) if primary_major[i] == 0],
+    '3y_1s': [lectures[4][i] for i in range(students) if primary_major[i] == 0],
+    '3y_2s': [lectures[5][i] for i in range(students) if primary_major[i] == 0],
+    '4y_1s': [lectures[6][i] for i in range(students) if primary_major[i] == 0],
+    '4y_2s': [lectures[7][i] for i in range(students) if primary_major[i] == 0]
+}
+
+df_all = pd.DataFrame(dataframe)
+df_primary = pd.DataFrame(dataframe_primary)
+df_not_primary = pd.DataFrame(dataframe_not_primary)
+
+# Code to debug above code (line 66~68)
 # print(df)
 # print(dataframe)
+# print(len(df))
 
 total_courses = set()
 
+# Find all lectures in 'dataframe'
 for semester in dataframe:
         if semester == 'student_ID': continue
         for stu_no in range(students):
@@ -92,7 +121,7 @@ def find_course_associations(df, first_year, first_year_cols, second_year_cols, 
 
     for (first, next_course), ratio in result.items():
         print(str(first_year) + "학년->" + str(first_year + 1) + "학년: " + f"'{first}'를 들은 학생 중 {ratio*100:.1f}%가 다음 해에 '{next_course}'를 수강함.")
-    
+
     return result
 
 # A function to find sequences of taking courses (on a semester-by-semester basis)
@@ -128,8 +157,12 @@ def find_semester_course_associations(df, from_semester, to_semester, total_cour
 
     return result
 
+# (1) For all students responded in our survey
+
+print("\n\n모든 학생들 대상 분석 결과(minsup = 3, mincoeff = 0.5)\n")
+
 year_result1to2 = find_course_associations(
-    df,
+    df_all,
     first_year=1,
     first_year_cols=['1y_1s', '1y_2s'],
     second_year_cols=['2y_1s', '2y_2s'],
@@ -137,7 +170,7 @@ year_result1to2 = find_course_associations(
 )
 
 year_result2to3 = find_course_associations(
-    df,
+    df_all,
     first_year=2,
     first_year_cols=['2y_1s', '2y_2s'],
     second_year_cols=['3y_1s', '3y_2s'],
@@ -145,17 +178,89 @@ year_result2to3 = find_course_associations(
 )
 
 year_result3to4 = find_course_associations(
-    df,
+    df_all,
     first_year=3,
     first_year_cols=['3y_1s', '3y_2s'],
     second_year_cols=['4y_1s', '4y_2s'],
     total_courses=total_courses
 )
 
-sem_result1to2 = find_semester_course_associations(df, '1y_1s', '1y_2s', total_courses)
-sem_result2to3 = find_semester_course_associations(df, '1y_2s', '2y_1s', total_courses)
-sem_result3to4 = find_semester_course_associations(df, '2y_1s', '2y_2s', total_courses)
-sem_result4to5 = find_semester_course_associations(df, '2y_2s', '3y_1s', total_courses)
-sem_result5to6 = find_semester_course_associations(df, '3y_1s', '3y_2s', total_courses)
-sem_result6to7 = find_semester_course_associations(df, '3y_2s', '4y_1s', total_courses)
-sem_result7to8 = find_semester_course_associations(df, '4y_1s', '4y_2s', total_courses)
+sem_result1to2 = find_semester_course_associations(df_all, '1y_1s', '1y_2s', total_courses)
+sem_result2to3 = find_semester_course_associations(df_all, '1y_2s', '2y_1s', total_courses)
+sem_result3to4 = find_semester_course_associations(df_all, '2y_1s', '2y_2s', total_courses)
+sem_result4to5 = find_semester_course_associations(df_all, '2y_2s', '3y_1s', total_courses)
+sem_result5to6 = find_semester_course_associations(df_all, '3y_1s', '3y_2s', total_courses)
+sem_result6to7 = find_semester_course_associations(df_all, '3y_2s', '4y_1s', total_courses)
+sem_result7to8 = find_semester_course_associations(df_all, '4y_1s', '4y_2s', total_courses)
+
+# (2) For main major of CSE students responded in our survey
+
+print("\n\n컴퓨터학과 본전공생 대상 분석 결과(minsup = 3, mincoeff = 0.5)\n")
+
+year_result1to2 = find_course_associations(
+    df_primary,
+    first_year=1,
+    first_year_cols=['1y_1s', '1y_2s'],
+    second_year_cols=['2y_1s', '2y_2s'],
+    total_courses=total_courses
+)
+
+year_result2to3 = find_course_associations(
+    df_primary,
+    first_year=2,
+    first_year_cols=['2y_1s', '2y_2s'],
+    second_year_cols=['3y_1s', '3y_2s'],
+    total_courses=total_courses
+)
+
+year_result3to4 = find_course_associations(
+    df_primary,
+    first_year=3,
+    first_year_cols=['3y_1s', '3y_2s'],
+    second_year_cols=['4y_1s', '4y_2s'],
+    total_courses=total_courses
+)
+
+sem_result1to2 = find_semester_course_associations(df_primary, '1y_1s', '1y_2s', total_courses)
+sem_result2to3 = find_semester_course_associations(df_primary, '1y_2s', '2y_1s', total_courses)
+sem_result3to4 = find_semester_course_associations(df_primary, '2y_1s', '2y_2s', total_courses)
+sem_result4to5 = find_semester_course_associations(df_primary, '2y_2s', '3y_1s', total_courses)
+sem_result5to6 = find_semester_course_associations(df_primary, '3y_1s', '3y_2s', total_courses)
+sem_result6to7 = find_semester_course_associations(df_primary, '3y_2s', '4y_1s', total_courses)
+sem_result7to8 = find_semester_course_associations(df_primary, '4y_1s', '4y_2s', total_courses)
+
+# (3) For double major of CSE students responded in our survey
+
+print("\n\n컴퓨터학과 이중/복수/융합/부전공공생 대상 분석 결과(minsup = 3, mincoeff = 0.5)\n")
+
+year_result1to2 = find_course_associations(
+    df_not_primary,
+    first_year=1,
+    first_year_cols=['1y_1s', '1y_2s'],
+    second_year_cols=['2y_1s', '2y_2s'],
+    total_courses=total_courses
+)
+
+year_result2to3 = find_course_associations(
+    df_not_primary,
+    first_year=2,
+    first_year_cols=['2y_1s', '2y_2s'],
+    second_year_cols=['3y_1s', '3y_2s'],
+    total_courses=total_courses
+)
+
+year_result3to4 = find_course_associations(
+    df_not_primary,
+    first_year=3,
+    first_year_cols=['3y_1s', '3y_2s'],
+    second_year_cols=['4y_1s', '4y_2s'],
+    total_courses=total_courses
+)
+
+sem_result1to2 = find_semester_course_associations(df_not_primary, '1y_1s', '1y_2s', total_courses)
+sem_result2to3 = find_semester_course_associations(df_not_primary, '1y_2s', '2y_1s', total_courses)
+sem_result3to4 = find_semester_course_associations(df_not_primary, '2y_1s', '2y_2s', total_courses)
+sem_result4to5 = find_semester_course_associations(df_not_primary, '2y_2s', '3y_1s', total_courses)
+sem_result5to6 = find_semester_course_associations(df_not_primary, '3y_1s', '3y_2s', total_courses)
+sem_result6to7 = find_semester_course_associations(df_not_primary, '3y_2s', '4y_1s', total_courses)
+sem_result7to8 = find_semester_course_associations(df_not_primary, '4y_1s', '4y_2s', total_courses)
