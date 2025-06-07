b527cc191274e20aea91d03446dd30fde5636525
Jackson Ang <jacksonang222@gmail.com>
2025-06-07 18:54:53 +0700

    Add function to compute frequent itemsets per semester

--- CoursePatterns/frequent_itemsets.py
+++ CoursePatterns/frequent_itemsets.py
@@ -1,10 +1,10 @@
-def get_frequent_itemsets(min_sup=0.5):
-    from preprocess_data import get_preprocessed_data
-    from mlxtend.preprocessing import TransactionEncoder
-    from mlxtend.frequent_patterns import apriori
-    import pandas as pd
+from preprocess_data import get_preprocessed_data
+from mlxtend.preprocessing import TransactionEncoder
+from mlxtend.frequent_patterns import apriori
+import pandas as pd
 
-    df = get_preprocessed_data("raw_data.xlsx", "course_name.json")
+def get_frequent_itemsets(min_sup=0.5):
+    df = get_preprocessed_data()
 
     transactions = df[df.columns[1:]].apply(
             lambda row: sorted(set(sum(row, []))), axis=1
@@ -18,3 +18,23 @@ def get_frequent_itemsets(min_sup=0.5):
     frequent_itemsets = frequent_itemsets.sort_values(by="support", ascending=False)
 
     return frequent_itemsets
+
+def get_frequent_itemsets_per_semester(min_sup=0.5):
+    df = get_preprocessed_data()
+    results = {}
+
+    for col in df.columns[1:]:
+        transactions = df[col].tolist()
+        if all(not row for row in transactions):
+            continue
+
+        te = TransactionEncoder()
+        te_ary = te.fit(transactions).transform(transactions)
+        df_encoded = pd.DataFrame(te_ary, columns=te.columns_)
+
+        frequent_itemsets = apriori(df_encoded, min_support=min_sup, use_colnames=True)
+        frequent_itemsets = frequent_itemsets.sort_values(by="support", ascending=False)
+
+        results[col] = frequent_itemsets
+
+    return results
